ee07d3cb65a72f257ca2b6eb62bd6e8bd13f7be8
Maksymiv96 <Maksymiv96@users.noreply.github.com>
2018-08-22 21:48:16 +0300

    upgrade border detection
    there was many imgs from board with reflection and blinking. i've tried work with adaptive threshold, and color mask. looks like is working fine.

--- src/cpp/cpp/Solver.cpp
+++ src/cpp/cpp/Solver.cpp
@@ -15,7 +15,7 @@ using namespace cv;
 using namespace std;
 
 //dont forget to insert folder with imgs to folder
-const string ImageFolder = "../../../imgs/board/";
+const string ImageFolder = "../../../imgs/paper/";
 const int GREEN = 1, RED = 2;
 
 Point color_points_detection(Mat *image, int color)
@@ -59,21 +59,38 @@ Point color_points_detection(Mat *image, int color)
 	else return fitEllipse(contours[0]).center;
 }
 
-Point* find_entry_and_exit(Mat image)
+void borderDetection(Mat *image, Mat *threshedImage, Mat *outImage)
 {
-	Point* points = new Point[2];
-	vector<vector<Point>> finded_countours_1;
-	vector<Vec4i> hierarchy_1;
-	findContours(image, finded_countours_1, hierarchy_1, RETR_TREE, CHAIN_APPROX_NONE);
-	//drawContours(image, finded_countours, -1, Scalar(255, 0, 0));
-	points[0] = fitEllipse(finded_countours_1[0]).center;
-	points[1] = fitEllipse(finded_countours_1[1]).center;
-	return points;
+	//func detect color dots and delete it from threshedImage, because adaptive threshold work pretty good and i think better work with it
+	Mat mask;
+	Scalar lower, upper;
+	Mat buf;
+	image->copyTo(buf);
+	cvtColor(buf, buf, COLOR_BGR2HSV);
+	Mat mask2;
+	lower = Scalar(0, 100, 80);
+	upper = Scalar(15, 255, 255);
+	inRange(buf, lower, upper, mask);
+	lower = Scalar(165, 100, 100);
+	upper = Scalar(180, 255, 255);
+	inRange(buf, lower, upper, mask2);
+	bitwise_or(mask, mask2, mask);
+
+	lower = Scalar(45, 100, 40);
+	upper = Scalar(80, 255, 255);
+	inRange(buf, lower, upper, mask2);
+	bitwise_or(mask, mask2, mask);
+	
+	mask = 255 - mask;
+
+	threshedImage->copyTo(*outImage, mask);
+	morphologyEx(*outImage, *outImage, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(3, 3)), Point(-1, -1), 1);
+	morphologyEx(*outImage, *outImage, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(3, 3)), Point(-1, -1), 3);
 }
 
 int main()
 {
-	Mat img = imread(ImageFolder + "paint.jpg");
+	Mat img = imread(ImageFolder + "round.jpg");
 
 	Mat blured_image;
 	Mat opened_image;
@@ -81,12 +98,13 @@ int main()
 	Mat thresh_image;
 
 	GaussianBlur(img, blured_image, Size(3, 3), 5, 3);
-	//bilateralFilter(img, blured_image, 1, 20, 5);
+	//bilateralFilter(img, blured_image, 1, 50, 50);
 	morphologyEx(blured_image, opened_image, MORPH_CLOSE, getStructuringElement(MORPH_ELLIPSE, Size(3, 3)), Point(-1, -1), 2);
 
 	cvtColor(opened_image, gray_image, CV_BGR2GRAY);
 	//threshold(gray_image, thresh_image, 100, 255, THRESH_BINARY_INV);
 	adaptiveThreshold(gray_image, thresh_image, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 11, 7);
+	morphologyEx(thresh_image, thresh_image, MORPH_CLOSE, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)), Point(-1, -1), 3);
 
 
 	////Creating mask
@@ -126,29 +144,10 @@ int main()
 	morphologyEx(mask, mask, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(3, 3)), Point(-1, -1), 2);
 	////
 
-
-	//Mat thresh;
-	//thresh_image.copyTo(thresh, mask);
-	//thresh.copyTo(thresh_image);
-	////bitwise_and(thresh_image, thresh_image, mask);
-	//morphologyEx(mask, mask, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(3, 3)), Point(-1, -1), 2);
-	//morphologyEx(thresh_image, thresh_image, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(3, 3)), Point(-1, -1), 1);
-
-
-
-
-
 	//Border detecting 
 	Mat BorderOnly;
-	img.copyTo(BorderOnly, mask);
-	cvtColor(BorderOnly, BorderOnly, COLOR_BGR2HSV);
-	inRange(BorderOnly, Scalar(0, 0, 0), Scalar(180, 100, 100), BorderOnly);
-	morphologyEx(BorderOnly, BorderOnly, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(3, 3)), Point(-1, -1), 1);
-	morphologyEx(BorderOnly, BorderOnly, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(3, 3)), Point(-1, -1), 1);
+	borderDetection(&img, &thresh_image, &BorderOnly);
 
-	//cv::inRange(img_copy, Scalar(127, 127, 127), Scalar(255, 255, 255), BorderOnly);
-
-	//PathFinder(&BorderOnly, color_points_detection(&blured_image, GREEN), color_points_detection(&blured_image, RED)).copyTo(BorderOnly);
 	vector<Point> path = PathFinder(&BorderOnly, color_points_detection(&blured_image, GREEN), color_points_detection(&blured_image, RED));
 	drawPathOnImage(&img, &path);
 
